973ce1b97b9f17e6269cec02863effcb1310644a
dengjinlong <dengjinlong@jtexpress.com>
2024-07-30 16:03:31 +0800

    配置一个导航堆栈

--- src/RootScreen.tsx
+++ src/RootScreen.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-// first, we need to wrap your app in NavigationContainer
-// NavigationContainer is a component that manages our navigation tree and contains the navigation state.
 import { NavigationContainer } from '@react-navigation/native';
-// createNativeStackNavigator is a function that returns an object containing 2 properties: Screen and Navigator
-// Both of them are React components used for configuring the navigator
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { TabScreen } from './TopTabScreen';
@@ -25,10 +21,15 @@ type RootStackParamList = {
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
+// screenOption  和 options 都是 NativeStackNavigationOptions 类型
+// screenOption : specify the same options for all of the screens in the navigator
+// component 必须是组件不能是函数
 function RootStack() {
   return (
-    <Stack.Navigator >
-      <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Navigator
+    initialRouteName={'Home'}
+    >
+      <Stack.Screen name="Home" component={HomeScreen} options={{title: 'Home'}} />
       <Stack.Screen name="MyTask" component={TabScreen} options={{ title: 'app_task' }} />
     </Stack.Navigator>
   );
@@ -41,3 +42,8 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+// first, we need to wrap your app in NavigationContainer
+// NavigationContainer is a component that manages our navigation tree and contains the navigation state.
+// createNativeStackNavigator is a function that returns an object containing 2 properties: Screen and Navigator
+// Both of them are React components used for configuring the navigator

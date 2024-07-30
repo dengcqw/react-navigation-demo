67ac22d431746b7b683898965f67ec22d988a0e8
dengjinlong <dengjinlong@jtexpress.com>
2024-07-30 15:41:39 +0800

    Creating a native stack navigator

--- src/RootScreen.tsx
+++ src/RootScreen.tsx
@@ -1,22 +1,43 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {TabScreen} from './TopTabScreen';
+import { View, Text } from 'react-native';
+// first, we need to wrap your app in NavigationContainer
+// NavigationContainer is a component that manages our navigation tree and contains the navigation state.
+import { NavigationContainer } from '@react-navigation/native';
+// createNativeStackNavigator is a function that returns an object containing 2 properties: Screen and Navigator
+// Both of them are React components used for configuring the navigator
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-type RootStackParamList = {
-  MyTask: undefined;
-};
+import { TabScreen } from './TopTabScreen';
 
-const RootStack = createNativeStackNavigator<RootStackParamList>();
 
-function MyTabs() {
+function HomeScreen() {
   return (
-    <NavigationContainer>
-      <RootStack.Navigator>
-        <RootStack.Screen name="MyTask" component={TabScreen} options={{title: 'app_task'}} />
-      </RootStack.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
   );
 }
 
-export default MyTabs;
+type RootStackParamList = {
+  MyTask: undefined
+  Home: undefined
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function RootStack() {
+  return (
+    <Stack.Navigator >
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="MyTask" component={TabScreen} options={{ title: 'app_task' }} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  );
+}

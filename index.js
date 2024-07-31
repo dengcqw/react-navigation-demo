98cc81cbaa8cc4fddf2f5d72e522e054c2a74391
dengjinlong <dengjinlong@jtexpress.com>
2024-07-31 11:03:31 +0800

    Drawer 依赖react-native-gesture-handler
    (cherry picked from commit 8e7744fadefc2e011aba960d1241eef1a8bff299)

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ import {AppRegistry} from 'react-native';
 //import App from './src/TabScreen';
 import App from './src/RootScreen';
 import {name as appName} from './app.json';
+import './gesture-handler';
 
 AppRegistry.registerComponent(appName, () => App);

9d3098977cd0a673d9575813a00fc180f4b1420b
dengjinlong <dengjinlong@jtexpress.com>
2024-08-05 11:13:57 +0800

    嵌套导航页面类型定义

--- src/BottomTabScreen.tsx
+++ src/BottomTabScreen.tsx
@@ -4,7 +4,23 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-function HomeScreen() {
+import type { RootStackParamList, BottomTabParamsList } from './RootStackParamList'
+import type { CompositeScreenProps } from '@react-navigation/native';
+import type { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+// 直接依赖的库，需要显示安装
+import type { StackScreenProps } from '@react-navigation/stack';
+
+// 定义嵌套的navigator的页面参数
+type HomeScreenProps = CompositeScreenProps<
+  BottomTabScreenProps<BottomTabParamsList, 'Home'>,
+  StackScreenProps<RootStackParamList>
+>;
+
+function HomeScreen({route, navigation}: HomeScreenProps) {
+  // stack 导航方法
+  //navigation.push()
+  // tab 导航方法
+  //navigation.jumpTo()
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home!</Text>
@@ -36,7 +52,7 @@ export default function RootTabs() {
             iconName = focused ? 'cart-outline' : 'cart';
           }
 
-          console.log('----> msg', iconName)
+          //console.log('----> msg', iconName)
           // You can return any component that you like here!
           return <Ionicons name={iconName} size={size} color={color} />;
         },
